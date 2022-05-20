96e2d50d57b64cb423e8a02bbc116a3cc5020ea4
David Reyes Avila <75442085+David-RA316184979@users.noreply.github.com>
2022-05-20 14:41:38 -0500

    Update Final.cpp

--- Proyecto_Final/Proyecto_Final/Final.cpp
+++ Proyecto_Final/Proyecto_Final/Final.cpp
@@ -951,13 +951,13 @@ int main()
 		staticShader.setMat4("model", model);
 		edificiosVilla.Draw(staticShader);
 
-		model = glm::translate(glm::mat4(1.0f), glm::vec3(398.0f, 0.0f, -510.0f));//edificio 6 de villa (ES EL QUE RELLENARÉ)
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(398.0f, 0.02f, -510.0f));//edificio 6 de villa (ES EL QUE RELLENARÉ)
 		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(8.0f));
 		staticShader.setMat4("model", model);
 		EdificioPlus.Draw(staticShader);
 
-		model = glm::translate(glm::mat4(1.0f), glm::vec3(398.0f, 0.0f, -518.0f));//Pared 
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(398.0f, 0.02f, -518.0f));//Pared 
 		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(1.0f, 0.6f, 0.3f));
 		staticShader.setMat4("model", model);
@@ -966,7 +966,7 @@ int main()
 		//Para que las ventanas sean transparentes
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		model = glm::translate(glm::mat4(1.0f), glm::vec3(398.0f, 0.0f, -510.0f));//VENTANA
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(398.0f, 0.02f, -510.0f));//VENTANA
 		model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(8.0f));
 		staticShader.setMat4("model", model);
