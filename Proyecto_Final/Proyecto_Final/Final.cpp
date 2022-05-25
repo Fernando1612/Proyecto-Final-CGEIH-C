b3093053d0ab21b74d12c4c2e2f56a8f97ebce25
salinasromerodaniel <42517271+salinasromerodaniel@users.noreply.github.com>
2022-05-25 10:22:30 -0600

    Puertada de entrada al depa
    colocada y escalada

--- Proyecto_Final/Proyecto_Final/Final.cpp
+++ Proyecto_Final/Proyecto_Final/Final.cpp
@@ -1362,6 +1362,12 @@ int main()
 		staticShader.setMat4("model", model);
 		puertab.Draw(staticShader);
 
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 9.2f, -74.5f));//puerta de entrada
+		model = glm::rotate(model, glm::radians(puertaInc), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(2.7f, 2.8f, 3.0f));
+		staticShader.setMat4("model", model);
+		puertab.Draw(staticShader);
+
 		model = glm::translate(glm::mat4(1.0f), glm::vec3(-10.0f, 10.0f, -21.0f));//base de la puerta
 		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(2.7f, 2.8f, 3.0f));
